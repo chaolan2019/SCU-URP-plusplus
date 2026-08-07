e9b860792f9004d87aa7dd384da6d824c7a2f728
chaolan2019 <2718748334@qq.com>
2026-08-07 19:38:03 +0800

    chore: 恢复 @connect 精确白名单（4 域名），README 明确首次确认次数（主+辅最多 6 次，一次性）

--- urppp.user.js
+++ urppp.user.js
@@ -15,6 +15,10 @@
 // @grant        GM_setValue
 // @grant        unsafeWindow
 // @grant        GM_xmlhttpRequest
+// @connect      raw.githubusercontent.com
+// @connect      github.com
+// @connect      cdn.jsdelivr.net
+// @connect      gh-proxy.com
 // @run-at       document-start
 // ==/UserScript==
 
