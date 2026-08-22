581928c7ca64b596dafe61f5b3afad9d75302ad1
chaolan2019 <2718748334@qq.com>
2026-08-22 20:22:37 +0800

    release: v1.8.2（主脚本）
    Fixed:
    - 校历弹窗：顶栏固定在窗口顶部（不悬浮），仅时间线独立滚动；editorial 主题下一个事件卡片去圆角
    - 清爽模式：点刷新不重播进入动画
    辅助插件维持 1.4.1

--- urppp.user.js
+++ urppp.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SCU URP++教务系统美化
 // @namespace    https://github.com/chaolan2019/SCU-URP-plusplus
-// @version      1.8.1
+// @version      1.8.2
 // @description  四川大学 URP 教务系统美化 + 清爽模式 | 课表/成绩/教室聚合
 // @author       Chao_Lan,Hanako
 // @license      GPL-3.0-only
@@ -16698,7 +16698,7 @@ ${arcs}
       }
     } catch (_) {
     }
-    const URPPP_VERSION = "1.8.1";
+    const URPPP_VERSION = "1.8.2";
     const URPPP_UPDATE = {
       mainRaw: "https://raw.githubusercontent.com/chaolan2019/SCU-URP-plusplus/main/urppp.user.js",
       assistRaw: "https://raw.githubusercontent.com/chaolan2019/SCU-URP-plusplus/main/urpppp.user.js",
